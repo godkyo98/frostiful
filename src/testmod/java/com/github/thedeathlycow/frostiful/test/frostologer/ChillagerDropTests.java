c66eed247224f724da730bf9e1fd217cd33e70f5
TheDeathlyCow <53499406+TheDeathlyCow@users.noreply.github.com>
2024-11-21 03:11:14 -1000

    Fix Chillager Captains not Dropping Ominious Bottles (#119)
    * add ominious bottle to chillager loot table
    
    * add random sequences to entity and gameplay loot tables
    
    * add chillager drop tests

---
+++ src/testmod/java/com/github/thedeathlycow/frostiful/test/frostologer/ChillagerDropTests.java
@@ -0,0 +1,52 @@
+package com.github.thedeathlycow.frostiful.test.frostologer;
+
+import com.github.thedeathlycow.frostiful.entity.ChillagerEntity;
+import com.github.thedeathlycow.frostiful.registry.FEntityTypes;
+import net.fabricmc.fabric.api.gametest.v1.FabricGameTest;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.damage.DamageSources;
+import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.test.GameTest;
+import net.minecraft.test.TestContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.village.raid.Raid;
+import net.minecraft.world.World;
+
+@SuppressWarnings("unused")
+public class ChillagerDropTests {
+    @GameTest(templateName = FabricGameTest.EMPTY_STRUCTURE)
+    public void regular_chillager_does_not_drop_ominous_bottle(TestContext context) {
+        World world = context.getWorld();
+        DamageSources damageSources = world.getDamageSources();
+
+        ChillagerEntity chillager = context.spawnEntity(FEntityTypes.CHILLAGER, BlockPos.ORIGIN);
+
+        chillager.damage(damageSources.genericKill(), Float.MAX_VALUE);
+
+        context.dontExpectItemAt(Items.OMINOUS_BOTTLE, BlockPos.ORIGIN, 3f);
+
+        context.complete();
+    }
+
+    @GameTest(templateName = FabricGameTest.EMPTY_STRUCTURE)
+    public void chillager_captain_drops_ominous_bottle(TestContext context) {
+        World world = context.getWorld();
+        DamageSources damageSources = world.getDamageSources();
+
+        ChillagerEntity chillager = context.spawnEntity(FEntityTypes.CHILLAGER, BlockPos.ORIGIN);
+        chillager.setPatrolLeader(true);
+        chillager.equipStack(
+                EquipmentSlot.HEAD,
+                Raid.getOminousBanner(chillager.getRegistryManager().getWrapperOrThrow(RegistryKeys.BANNER_PATTERN))
+        );
+
+        context.assertTrue(chillager.isCaptain(), "Chillager is not a captain!");
+
+        chillager.damage(damageSources.genericKill(), Float.MAX_VALUE);
+
+        context.expectItemAt(Items.OMINOUS_BOTTLE, BlockPos.ORIGIN, 3f);
+
+        context.complete();
+    }
+}
