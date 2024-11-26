2d9f3fbaa415b2a480b9a14d59640c7c4e0d639b
TheDeathlyCow <53499406+TheDeathlyCow@users.noreply.github.com>
2024-11-26 02:24:23 -1000

    Fix Powder Snow Walkable mixin compatibility (#122)
    * remove always-cancelling call
    
    * add separate method for disabling hard-coded vanilla leather boots check
    
    * only let powder snow walkable tag work for the feet and body slots

--- src/main/java/com/github/thedeathlycow/frostiful/mixins/block/PowderSnowWalkableMixin.java
+++ src/main/java/com/github/thedeathlycow/frostiful/mixins/block/PowderSnowWalkableMixin.java
@@ -1,37 +1,49 @@
 package com.github.thedeathlycow.frostiful.mixins.block;
 
 import com.github.thedeathlycow.frostiful.registry.tag.FItemTags;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.block.PowderSnowBlock;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.registry.tag.EntityTypeTags;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PowderSnowBlock.class)
-public class PowderSnowWalkableMixin {
+public abstract class PowderSnowWalkableMixin {
 
+    @Unique
+    private static final EquipmentSlot[] ALLOWED_SLOTS = new EquipmentSlot[]{EquipmentSlot.FEET, EquipmentSlot.BODY};
+
+    @ModifyReturnValue(
+            method = "canWalkOnPowderSnow",
+            at = @At(
+                    value = "RETURN",
+                    ordinal = 1
+            )
+    )
+    private static boolean ignoreHardCodedLeatherBootsCheck(boolean original) {
+        return false;
+    }
 
     @Inject(
             method = "canWalkOnPowderSnow",
             at = @At("HEAD"),
             cancellable = true
     )
-    private static void canWalkOnPowderSnowBootTag(Entity entity, CallbackInfoReturnable<Boolean> cir) {
-        if (!entity.getType().isIn(EntityTypeTags.POWDER_SNOW_WALKABLE_MOBS) && (entity instanceof LivingEntity livingEntity)) {
-
-            for (ItemStack stack : livingEntity.getArmorItems()) {
+    private static void checkPowderSnowWalkableItemTag(Entity entity, CallbackInfoReturnable<Boolean> cir) {
+        if (entity instanceof LivingEntity livingEntity) {
+            for (EquipmentSlot slot : ALLOWED_SLOTS) {
+                ItemStack stack = livingEntity.getEquippedStack(slot);
                 if (!stack.isEmpty() && stack.isIn(FItemTags.POWDER_SNOW_WALKABLE)) {
                     cir.setReturnValue(true);
                     return;
                 }
             }
-
-            cir.setReturnValue(false);
         }
     }
-
 }
