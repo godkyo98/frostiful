f2f50a70880a5d4494244ca74385fa3bc90a51a8
TheDeathlyCow <thedeathlycow@gmail.com>
2025-03-21 23:42:00 -1000

    update chillager

--- src/client/java/com/github/thedeathlycow/frostiful/client/render/entity/ChillagerEntityRenderer.java
+++ src/client/java/com/github/thedeathlycow/frostiful/client/render/entity/ChillagerEntityRenderer.java
@@ -9,23 +9,31 @@ import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.IllagerEntityRenderer;
 import net.minecraft.client.render.entity.feature.HeldItemFeatureRenderer;
 import net.minecraft.client.render.entity.model.IllagerEntityModel;
+import net.minecraft.client.render.entity.state.EntityRenderState;
+import net.minecraft.client.render.entity.state.IllagerEntityRenderState;
+import net.minecraft.client.render.entity.state.LivingEntityRenderState;
 import net.minecraft.util.Identifier;
 
 @Environment(EnvType.CLIENT)
-public class ChillagerEntityRenderer extends IllagerEntityRenderer<ChillagerEntity> {
+public class ChillagerEntityRenderer extends IllagerEntityRenderer<ChillagerEntity, IllagerEntityRenderState> {
 
     private static final Identifier TEXTURE = Frostiful.id("textures/entity/illager/chillager.png");
 
 
     public ChillagerEntityRenderer(EntityRendererFactory.Context context) {
         super(context, new IllagerEntityModel<>(context.getPart(FEntityModelLayers.CHILLAGER)), 0.5F);
-        this.addFeature(new HeldItemFeatureRenderer<>(this, context.getHeldItemRenderer()));
+        this.addFeature(new HeldItemFeatureRenderer<>(this, context.getItemRenderer()));
 
         this.model.getHat().visible = true;
     }
 
     @Override
-    public Identifier getTexture(ChillagerEntity entity) {
+    public IllagerEntityRenderState createRenderState() {
+        return new IllagerEntityRenderState();
+    }
+
+    @Override
+    public Identifier getTexture(IllagerEntityRenderState state) {
         return TEXTURE;
     }
 }
