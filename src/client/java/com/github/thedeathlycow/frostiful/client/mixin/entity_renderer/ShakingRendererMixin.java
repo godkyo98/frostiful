0990f6a40d218b4838bdde384d618b05505f2f21
TheDeathlyCow <thedeathlycow@gmail.com>
2025-03-22 15:43:44 -1000

    update shaking renderer

--- src/client/java/com/github/thedeathlycow/frostiful/client/mixin/entity_renderer/ShakingRendererMixin.java
+++ src/client/java/com/github/thedeathlycow/frostiful/client/mixin/entity_renderer/ShakingRendererMixin.java
@@ -5,24 +5,24 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.render.entity.model.EntityModel;
+import net.minecraft.client.render.entity.state.LivingEntityRenderState;
 import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntityRenderer.class)
 @Environment(EnvType.CLIENT)
-public class ShakingRendererMixin<T extends LivingEntity, M extends EntityModel<T>> {
-
+public class ShakingRendererMixin<T extends LivingEntity, S extends LivingEntityRenderState, M extends EntityModel<? super S>> {
     @Inject(
-            method = "isShaking",
-            at = @At("TAIL"),
-            cancellable = true
+            method = "updateRenderState(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/client/render/entity/state/LivingEntityRenderState;F)V",
+            at = @At("TAIL")
     )
-    private void shakeWhenFreezing(T entity, CallbackInfoReturnable<Boolean> cir) {
+    private void shakeWhenFreezing(T entity, S state, float tickDelta, CallbackInfo ci) {
         if (SurvivalUtils.isShiveringRender(entity)) {
-            cir.setReturnValue(true);
+            state.shaking = true;
         }
     }
 }
