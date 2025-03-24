a128e54512f78628d0c1dc3e436f090fc2acd0d5
TheDeathlyCow <thedeathlycow@gmail.com>
2025-03-24 01:12:49 -1000

    reset structure files to test

--- src/main/java/com/github/thedeathlycow/frostiful/datafix/StructureUpdateHelper.java
+++ src/main/java/com/github/thedeathlycow/frostiful/datafix/StructureUpdateHelper.java
@@ -5,8 +5,12 @@ import com.mojang.datafixers.DataFixer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.SharedConstants;
+import net.minecraft.data.DataWriter;
+import net.minecraft.data.dev.NbtProvider;
+import net.minecraft.data.validate.StructureValidatorProvider;
 import net.minecraft.datafixer.DataFixTypes;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtHelper;
 import net.minecraft.nbt.NbtIo;
 import net.minecraft.nbt.NbtSizeTracker;
 
@@ -22,17 +26,9 @@ public class StructureUpdateHelper {
     // relative to the /run dir
     private static final Path STRUCTURE_PATH = Paths.get("../src/main/resources/data/frostiful/structure/");
 
-    private static final int TARGET_VERSION = SharedConstants.getGameVersion().getSaveVersion().getId();
-
-    private final DataFixer dataFixer;
-
-    public StructureUpdateHelper(DataFixer dataFixer) {
-        this.dataFixer = dataFixer;
-    }
-
     public static void initialize() {
         ServerLifecycleEvents.SERVER_STARTED.register(server -> {
-            var updater = new StructureUpdateHelper(server.getDataFixer());
+            var updater = new StructureUpdateHelper();
             updater.updateAllStructures();
         });
     }
@@ -53,15 +49,13 @@ public class StructureUpdateHelper {
 
     private void updateStructureFile(Path path) {
         try (InputStream in = Files.newInputStream(path)) {
-            NbtCompound tag = NbtIo.readCompressed(in, NbtSizeTracker.ofUnlimitedBytes());
-            int oldVersion = tag.contains("DataVersion") ? tag.getInt("DataVersion") : TARGET_VERSION;
+            NbtCompound oldNbt = NbtIo.readCompressed(in, NbtSizeTracker.ofUnlimitedBytes());
 
-            NbtCompound updatedTag = DataFixTypes.STRUCTURE.update(this.dataFixer, tag, oldVersion, TARGET_VERSION);
-
-            updatedTag.putInt("DataVersion", TARGET_VERSION);
+            NbtCompound updatedNbt = StructureValidatorProvider.update(path.toString(), oldNbt);
 
             try (OutputStream out = Files.newOutputStream(path)) {
-                NbtIo.writeCompressed(updatedTag, out);
+                NbtIo.writeCompressed(updatedNbt, out);
+                Frostiful.LOGGER.info("Updated structure {}", path);
             }
         } catch (IOException e) {
             Frostiful.LOGGER.error("Failed to update structure file {}: {}", path, e);
